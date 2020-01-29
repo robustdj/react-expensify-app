9c42ffde9992c27901e0a06a5f248c36bf0a476a
Derrick Camerino <robustdj@gmail.com>
2020-01-28 17:06:37 -0800

    Fetch data from firebase on app render

--- src/tests/reducers/expenses.test.js
+++ src/tests/reducers/expenses.test.js
@@ -66,3 +66,25 @@ test('should not edit an expense if expense not found', () => {
   const state = expensesReducer(expenses, action)
   expect(state).toEqual(expenses)
 })
+
+test('should set expenses', () => {
+  // const expense = {
+  //   id: '200',
+  //   description: 'Paper',
+  //   note: 'For the office',
+  //   amount: 200,
+  //   createdAt: 1
+  // }
+  // const addExpenseAction = {
+  //   type: 'ADD_EXPENSE',
+  //   expense 
+  // }
+  // expensesReducer(expense, addExpenseAction)
+
+  const action = {
+    type: 'SET_EXPENSES',
+    expenses: [expenses[1]]
+  }
+  const state = expensesReducer(expenses, action)
+  expect(state).toEqual([expenses[1]])
+})
